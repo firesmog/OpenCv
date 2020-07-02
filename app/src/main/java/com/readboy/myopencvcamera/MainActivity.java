c7269d7ec9c4dfb94e90525fb13129cc44c7fe87
廖泽宇 <firesmog@163.com>
2020-07-02 11:57:50 +0800

    1.导入opencv moudle 2.安装必备的manager apk到设备 3.初步适用灰度图片

---
+++ app/src/main/java/com/readboy/myopencvcamera/MainActivity.java
@@ -0,0 +1,100 @@
+package com.readboy.myopencvcamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView img_after;
+    TextView text_togray;
+    Bitmap srcBitmap;
+    Bitmap grayBitmap;
+    private static boolean flag = true;
+    //private static boolean isFirst = true;
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img_after=(ImageView)findViewById(R.id.imageView_after);
+        text_togray=(TextView)findViewById(R.id.textView_togray);
+        text_togray.setOnClickListener(this);
+
+    }
+    //OpenCV库加载并初始化成功后的回调函数
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+
+        @Override
+        public void onManagerConnected(int status) {
+            // TODO Auto-generated method stub
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:
+                    Log.i(TAG, "成功加载");
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "成功加载！", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    Log.i(TAG, "加载失败");
+                    Toast toast1 = Toast.makeText(getApplicationContext(),
+                            "加载失败！", Toast.LENGTH_LONG);
+                    toast1.setGravity(Gravity.CENTER, 0, 0);
+                    toast1.show();
+                    break;
+            }
+
+        }
+    };
+
+    public void procSrc2Gray(){
+        Mat rgbMat = new Mat();
+        Mat grayMat = new Mat();
+        srcBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        grayBitmap = Bitmap.createBitmap(srcBitmap.getWidth(), srcBitmap.getHeight(), Bitmap.Config.RGB_565);
+        Utils.bitmapToMat(srcBitmap, rgbMat);//convert original bitmap to Mat, R G B.
+        Imgproc.cvtColor(rgbMat, grayMat, Imgproc.COLOR_RGB2GRAY);//rgbMat to gray grayMat
+        Utils.matToBitmap(grayMat, grayBitmap); //convert mat to bitmap
+        Log.i(TAG, "procSrc2Gray sucess...");
+    }
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.textView_togray:
+                procSrc2Gray();
+                img_after.setImageBitmap(grayBitmap) ;
+                break;
+        }
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+}
