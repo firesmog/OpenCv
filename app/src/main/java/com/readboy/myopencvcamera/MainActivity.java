d9a0dfa2a4edd36e4a53c62006d3976b4906b9b8
廖泽宇 <firesmog@163.com>
2020-07-07 16:58:51 +0800

    1.这里截取到的蓝色区域确实为目标裁剪区域

--- app/src/main/java/com/readboy/myopencvcamera/MainActivity.java
+++ app/src/main/java/com/readboy/myopencvcamera/MainActivity.java
@@ -13,6 +13,7 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
@@ -21,7 +22,13 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
@@ -44,6 +51,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
 import static org.opencv.imgproc.Imgproc.THRESH_BINARY_INV;
@@ -51,7 +59,8 @@ import static org.opencv.imgproc.Imgproc.THRESH_BINARY_INV;
 
 public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
     private static final String TAG = "TAG_CameraActivity";
-
+    private int WIDTH_BLOCK = 40;
+    private int HEIGHT_BLOCK = 40;
     private CameraBridgeViewBase mOpenCvCameraView;
     private boolean mIsJavaCamera = true;
     private MenuItem mItemSwitchCamera = null;
@@ -170,7 +179,9 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                 finish();
                 break;
             case R.id.clickItem:
-                savePicture(mRgba);
+                 //savePicture(mRgba);
+                //showCropPicture(mRgba);
+                showAutoCropPicture(mRgba);
                 ivShow.setVisibility(View.VISIBLE);
                 mOpenCvCameraView.setVisibility(View.GONE);
                 mViewMode = VIEW_MODE_CLICK;
@@ -221,11 +232,10 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     private Mat processImage( Mat gray ) {
         Mat b = new Mat();
         //高斯模糊效果较好，size里的参数只能为奇数
-        Imgproc.GaussianBlur(gray,b, new Size(3,3),0);
+        Imgproc.GaussianBlur(gray,b, new Size(7,7),0);
        // Imgproc.medianBlur( gray, b, 3);
         Mat t = new Mat();
         Imgproc.threshold(b, t, 125, 300, THRESH_BINARY);
-
         return t;
     }
 
@@ -251,7 +261,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                     int contourSize= (int) temp_contour.total();
                     MatOfPoint2f approxCurve_temp=new MatOfPoint2f();
                     //对图像轮廓点进行多边形拟合
-                    Imgproc.approxPolyDP(new_mat,approxCurve_temp,contourSize*0.04,true);
+                    Imgproc.approxPolyDP(new_mat,approxCurve_temp,contourSize*0.07,true);
                     if (approxCurve_temp.total()==4){
                         maxArea=contourarea;
                         maxAreaIdx=idx;
@@ -307,23 +317,9 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     }
 
     private void savePicture(Mat frame){
-       /* if(hasSaved){
-            return;
-        }
-        hasSaved = true;*/
         Mat frameData = processImage(frame);
         List<Mat> mats = new ArrayList<>();
         //多通道分离出单通道
-       /* Core.split(frameData,mats);
-        if( mats.size() > 0){
-            for (Mat mat : mats) {
-                List<Point> points = getCornersByContour(mat);
-                for (Point point : points) {
-                    Log.d(TAG,"point ======" + point.toString());
-                }
-            }
-        }*/
-
         Bitmap bitmap = Bitmap.createBitmap(frameData.width(), frameData.height(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(frameData, bitmap);
         String name = System.currentTimeMillis() + "output_image.jpg";
@@ -345,54 +341,152 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         int startY = (int) Math.min(leftTop.y,righttop.y);
         int maxWidth = edge.width() - startX;
         int maxHeight = edge.height() - startY;
+        Utils.matToBitmap(frame, bitmap);
         Bitmap rectBitmap = Bitmap.createBitmap(bitmap,startX,startY ,
                 Math.min((int) Math.abs(Math.max((righttop.x - leftTop.x),rightbottom.x - leftbottom.x)),maxWidth),
                 Math.min((int)Math.abs( Math.max((leftbottom.y - leftTop.y),rightbottom.y - righttop.y)),maxHeight));
         ivShow.setImageBitmap(rectBitmap);
-        //width = 1440,height = 2768
-      /*  try {
-            outputStream = new FileOutputStream(fileName);
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-            outputStream.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            if(outputStream != null){
-                try {
-                    outputStream.close();
-                    Log.d(TAG,"savePicture end");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    }
+
+    //将二值化后抓取的边框轮廓图片用不同颜色显示出来
+    private void showCropPicture(Mat frame){
+        Mat frameData = processImage(frame);
+        Bitmap bitmap = Bitmap.createBitmap(frameData.width(), frameData.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(frameData, bitmap);
+        String name = System.currentTimeMillis() + "output_image.jpg";
+        String pathResult = getExternalFilesDir("Pictures").getPath() + "/" + name;
+        String fileName = pathResult + ".jpg";
+        Imgcodecs.imwrite(fileName, frameData);
+        //ivShow.setImageBitmap(bitmap);
+        Mat edge=new Mat();
+        Mat contours=new Mat();
+        Imgproc.Canny(frameData,edge,90,270,5,true);
+        List<MatOfPoint> contourList=new ArrayList<>();
+        contours.create(edge.rows(), edge.cols(), CvType.CV_8UC3);
+        Imgproc.findContours(edge,contourList,new Mat(),Imgproc.RETR_LIST,Imgproc.CHAIN_APPROX_SIMPLE);
+        for (int i = 0; i < contourList.size(); i++) {
+            Random r = new Random();
+            Imgproc.drawContours(
+                    contours,
+                    contourList,
+                    i,
+                    new Scalar(r.nextInt(255), r.nextInt(255), r.nextInt(255)),
+                    -1
+            );
+        }
+        Utils.matToBitmap(contours, bitmap);
+        ivShow.setImageBitmap(bitmap);
+    }
+
+    //自动按边框裁剪后拉伸
+    private void showAutoCropPicture(Mat frame){
+        Mat frameData = processImage(frame);
+        Bitmap bitmap = Bitmap.createBitmap(frameData.width(), frameData.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(frameData, bitmap);
+        Mat edge=new Mat();
+        Imgproc.Canny(frameData,edge,90,270,5,true);
+        List<Point> points = getCornersByContour(edge);
+        for (Point point : points) {
+            Log.d(TAG,"point ======" + point.toString() + ",width = " +edge.width() + ",height = " + edge.height());
+        }
+        Point bitmapTopLeft = points.get(0);
+        Point bitmapTopRight=points.get(1);
+        Point bitmapBottomLeft=points.get(2);
+        Point bitmapBottomRight=points.get(3);
+
+        Utils.matToBitmap(frame, bitmap);
+        Canvas canvas = new Canvas(bitmap);
+
+        Paint paint = new Paint();
+        paint.setColor(getResources().getColor(R.color.colorPrimary));
+        // 1. draw path
+        Path path = new Path();
+        path.moveTo((float) bitmapTopLeft.x, (float) bitmapTopLeft.y);
+        path.lineTo((float) bitmapTopRight.x, (float) bitmapTopRight.y);
+        path.lineTo((float) bitmapBottomRight.x, (float) bitmapBottomRight.y);
+        path.lineTo((float)bitmapBottomLeft.x,(float) bitmapBottomLeft.y);
+        path.moveTo((float)bitmapTopLeft.x, (float)bitmapTopLeft.y);
+
+        path.close();
+        canvas.drawPath(path, paint);
+
+        // 2. draw original bitmap
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        //canvas.drawBitmap(bitmap, 0, 0, paint);
+        Rect cropRect = new Rect(
+                Math.min((int)bitmapTopLeft.x, (int)bitmapBottomLeft.x),
+                Math.min((int)bitmapTopLeft.y, (int)bitmapTopRight.y),
+                Math.max((int)bitmapBottomRight.x, (int)bitmapTopRight.x),
+                Math.max((int)bitmapBottomRight.y, (int)bitmapBottomLeft.y));
+
+        Bitmap cut = Bitmap.createBitmap(
+                bitmap,
+                cropRect.left,
+                cropRect.top,
+                cropRect.width(),
+                cropRect.height()
+        );
+
+        /*android.graphics.Point cutTopLeft = new android.graphics.Point();
+        android.graphics.Point cutTopRight = new android.graphics.Point();
+        android.graphics.Point cutBottomLeft = new android.graphics.Point();
+        android.graphics.Point cutBottomRight = new android.graphics.Point();
+
+        cutTopLeft.x = (int)(bitmapTopLeft.x > bitmapBottomLeft.x ? bitmapTopLeft.x - bitmapBottomLeft.x : 0);
+        cutTopLeft.y = (int)( bitmapTopLeft.y > bitmapTopRight.y ? bitmapTopLeft.y - bitmapTopRight.y : 0);
+
+        cutTopRight.x = (int)(bitmapTopRight.x > bitmapBottomRight.x ? cropRect.width() : cropRect.width() - Math.abs(bitmapBottomRight.x - bitmapTopRight.x));
+        cutTopRight.y = (int)(bitmapTopLeft.y > bitmapTopRight.y ? 0 : Math.abs(bitmapTopLeft.y - bitmapTopRight.y));
+
+        cutBottomLeft.x = (int)(bitmapTopLeft.x > bitmapBottomLeft.x ? 0 : Math.abs(bitmapTopLeft.x - bitmapBottomLeft.x));
+        cutBottomLeft.y = (int)(bitmapBottomLeft.y > bitmapBottomRight.y ? cropRect.height() : cropRect.height() - Math.abs(bitmapBottomRight.y - bitmapBottomLeft.y));
+
+        cutBottomRight.x = (int)(bitmapTopRight.x > bitmapBottomRight.x ? cropRect.width() - Math.abs(bitmapBottomRight.x - bitmapTopRight.x) : cropRect.width());
+        cutBottomRight.y = (int)(bitmapBottomLeft.y > bitmapBottomRight.y ? cropRect.height() - Math.abs(bitmapBottomRight.y - bitmapBottomLeft.y) : cropRect.height());
+
+        Log.e("stk", cut.getWidth() + "x" + cut.getHeight());
+
+        Log.e("stk", "cutPoints="
+                + cutTopLeft.toString() + " "
+                + cutTopRight.toString() + " "
+                + cutBottomRight.toString() + " "
+                + cutBottomLeft.toString() + " ");
+
+        float width = cut.getWidth();
+        float height = cut.getHeight();
+
+        float[] src = new float[]{cutTopLeft.x, cutTopLeft.y, cutTopRight.x, cutTopRight.y, cutBottomRight.x, cutBottomRight.y, cutBottomLeft.x, cutBottomLeft.y};
+        float[] dst = new float[]{0, 0, width, 0, width, height, 0, height};
+
+        Matrix matrix = new Matrix();
+        matrix.setPolyToPoly(src, 0, dst, 0, 4);
+        Bitmap stretch = Bitmap.createBitmap(cut.getWidth(), cut.getHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas stretchCanvas = new Canvas(stretch);
+//            stretchCanvas.drawBitmap(cut, matrix, null);
+        stretchCanvas.concat(matrix);
+        stretchCanvas.drawBitmapMesh(cut, WIDTH_BLOCK, HEIGHT_BLOCK, generateVertices(cut.getWidth(), cut.getHeight()), 0, null, 0, null);*/
+
+        ivShow.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        ivShow.setImageBitmap(cut);
+    }
+
+
+    private float[] generateVertices(int widthBitmap, int heightBitmap) {
+
+        float[] vertices=new float[(WIDTH_BLOCK+1)*(HEIGHT_BLOCK+1)*2];
+
+        float widthBlock = (float)widthBitmap/WIDTH_BLOCK;
+        float heightBlock = (float)heightBitmap/HEIGHT_BLOCK;
+
+        for(int i=0;i<=HEIGHT_BLOCK;i++)
+            for(int j=0;j<=WIDTH_BLOCK;j++) {
+                vertices[i * ((HEIGHT_BLOCK+1)*2) + (j*2)] = j * widthBlock;
+                vertices[i * ((HEIGHT_BLOCK+1)*2) + (j*2)+1] = i * heightBlock;
             }
-        }*/
+        return vertices;
     }
 
-    /**
-     * 将屏幕中的位置（大小）对应到图片中
-     *
-     * @param w 屏幕中的位置（宽度）
-     * @param h 屏幕中的位置（高度）
-     * @return
-     */
-    private Point createCenterPictureRect(Mat edge, int h,int w) {
-        Display display = getWindowManager().getDefaultDisplay();
-        int wScreen = display.getWidth();
-        int hScreen = display.getHeight();
-
-
-        int wSavePicture = edge.width(); //因为图片旋转了，所以此处宽高换位
-        int hSavePicture = edge.height(); //因为图片旋转了，所以此处宽高换位
-
-        float wRate = (float) (wSavePicture) / (float) (wScreen);
-        float hRate = (float) (hSavePicture) / (float) (hScreen);
-        float rate = (wRate <= hRate) ? wRate : hRate;//也可以按照最小比率计算
-
-        int wRectPicture = (int) (w * wRate);
-        int hRectPicture = (int) (h * hRate);
-
-        return new Point(wRectPicture, hRectPicture);
-    }
 }
 
 
